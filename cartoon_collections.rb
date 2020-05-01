41ac3ffc148a183983488be3dd46cd7369db2e72
Maysoon Tabali <mtab03@me.com>
2020-05-01 16:17:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 #Function prints out each name in numbered (index) order starting at 1
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |name, index| #pass block with two arguments
+  dwarves.each_with_index do |name, index|
     puts "#{index + 1} #{name}" #returns indexed list of names starting at 1
   end
 end
