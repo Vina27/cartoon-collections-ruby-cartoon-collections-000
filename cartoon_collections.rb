e0a0c297e2cab490b481888eaafb0d9292a92aaf
Maysoon Tabali <mtab03@me.com>
2020-05-01 16:23:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,13 +20,13 @@ def long_planeteer_calls(calls)# code an argument here
   end
 end
 
-#Function .finds 1st string that includes cheese in the array 
+#Function .finds 1st string that includes cheese in the array
 def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   #Function finds first string that includes cheese in the array
   cheese.find do |cheese|
-  #function returns true if the given string contains the given string or nil
+  #function returns true if the given string contains the given string 
   cheese_types.include?(cheese)
  end
 end
