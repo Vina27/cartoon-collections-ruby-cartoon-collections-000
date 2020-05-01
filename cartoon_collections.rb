a4eeb1f6d115aa1a6e5ef388eae4d7cf5b96c4ff
Maysoon Tabali <mtab03@me.com>
2020-05-01 17:17:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,3 +30,8 @@ def find_the_cheese(cheese)# code an argument here
   cheese_types.include?(cheese)
  end
 end
+
+def start_with_B(words)
+  words = [boats, birds, tree].select {|word| word.first_letter}
+  word.first_letter == B 
+  words.select
